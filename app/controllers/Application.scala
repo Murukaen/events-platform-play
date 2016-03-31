44ad3a130b5b5f68d36fbb90ed5cf2bb25ecdb3c
razvan <razzvan.savu@gmail.com>
2016-03-31 07:44:10 +0300

    Show some hardcoded events

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,23 @@
 package controllers
 
+import models.Event
+import org.joda.time.DateTime
 import play.api._
 import play.api.mvc._
 
 class Application extends Controller {
 
+  def getDummyEvents = {
+    Seq(
+      new Event("Concert Bla-Bla", DateTime.now, "Casino"),
+      new Event("Vernisaj", DateTime.now, "Muzeul de Arta"),
+      new Event("Targ Something", DateTime.now, "Piata Unirii")
+    )
+  }
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready, boss."))
+    val events = getDummyEvents
+    Ok(views.html.index("Your new application is ready, boss.")(events))
   }
 
 }
---
+++ app/models/Event.scala
@@ -0,0 +1,10 @@
+package models
+
+import org.joda.time.DateTime
+
+/**
+  * Created by razvan on 30.03.2016.
+  */
+case class Event(name:String, time:DateTime, location:String) {
+
+}
